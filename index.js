7eae08c094a48814dd3508915b3587e621d56034
Viet Thuan <vietthuan98@gmail.com>
2020-06-16 12:21:34 +0700

    fix the view engine settings, navbar using  bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
 const db = mongoose.connection;
@@ -9,14 +10,14 @@ db.once('open', () => console.log('Connected to db'));
 
 const app = express();
 
+//view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 //static file
 app.use(express.static(path.join(__dirname, 'public')));
-//view engine
-app.set('view', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.render('index');
 })
 
 app.listen(3000, () => console.log('App listening at http://localhost:'+ 3000));
